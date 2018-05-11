03d9c3b273b0310fea8d59331cea3e6554377184
wanglolly <wanglolly@gmail.com>
2018-05-11 20:55:11 +0800

    Debug CVAE.py

--- Scripts/CVAE.py
+++ Scripts/CVAE.py
@@ -104,7 +104,7 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.mse_loss(recon_x, x.view(-1, 784), size_average=False)
+    BCE = F.mse_loss(recon_x, x, size_average=False)
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
