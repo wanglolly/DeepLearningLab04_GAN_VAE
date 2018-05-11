2e486ddd2cb95a837a9de93606653a85d7d6529c
wanglolly <wanglolly@gmail.com>
2018-05-11 15:26:41 +0800

    Initialize CVAE.py infoGAN.py

---
+++ Scripts/CVAE.py
@@ -0,0 +1,161 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from utils import idx2onehot
+
+
+parser = argparse.ArgumentParser(description='CVAE MNIST Example')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+
+torch.manual_seed(args.seed)
+
+device = torch.device("cuda" if args.cuda else "cpu")
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+
+class CVAE(nn.Module):   
+    def __init__(self):
+        super(CVAE, self).__init__()
+        #encoder
+        self.conv1 = nn.Sequential(
+                    nn.Conv2d(11, 3, kernel_size=3, stride = 1, padding = 1),
+                    nn.ReLU(),
+                    nn.Conv2d(3, 1, kernel_size=3, stride = 1, padding = 1),
+                    nn.ReLU())
+        self.fc1 = nn.Sequential(nn.Linear(in_features = 784, out_features = 400, bias = True),
+                                nn.ReLU())
+        self.fc21 = nn.Linear(in_features = 400, out_features = 20, bias = True)
+        self.fc22 = nn.Linear(in_features = 400, out_features = 20, bias = True)
+
+        #decoder
+        self.fc3 = nn.Sequential(nn.Linear(in_features = 30, out_features = 392, bias = True),
+                                nn.ReLU())
+        self.conv2 = nn.Sequential(
+                    nn.Conv2d(2, 11, kernel_size=3, stride = 1, padding = 1),
+                    nn.ReLU(),
+                    nn.UpsamplingNearest2d(scale_factor=2),
+                    nn.Conv2d(11, 3, kernel_size=3, stride = 1, padding = 1),
+                    nn.ReLU(),
+                    nn.Conv2d(3, 1, kernel_size=3, stride = 1, padding = 1),
+                    nn.Sigmoid())
+
+    def encode(self, x, c):
+        c = idx2onehot(c, n=10)
+        x = torch.cat((x, c), dim=-1)
+        h1 = self.fc1(self.conv1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5*logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z, c):
+        c = idx2onehot(c, n=10)
+        z = torch.cat((z, c), dim=-1)
+        return self.conv2(self.fc3(z))
+
+    def forward(self, x, c):
+        mu, logvar = self.encode(x.view(-1, 784), c)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z, c), mu, logvar, z
+
+
+model = CVAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.mse_loss(recon_x, x.view(-1, 784), size_average=False)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, label) in enumerate(train_loader):
+        #get data & label
+        data = data.to(device)
+        label = label.to(device)
+        data = data.view(-1, 784)
+        label = label.view(-1, 1)
+
+        optimizer.zero_grad()
+        recon_batch, mu, logvar, z = model(data, label)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                save_image(comparison.cpu(),
+                         '../CVAE_Results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
+    test(epoch)
+    with torch.no_grad():
+        sample = torch.randn(64, 20).to(device)
+        sample = model.decode(sample).cpu()
+        save_image(sample.view(64, 1, 28, 28),
+                   '../CVAE_Results/sample_' + str(epoch) + '.png')
