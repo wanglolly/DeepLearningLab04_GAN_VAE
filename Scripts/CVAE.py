f60942680afe15fda353aae652c96bcc8d01d1ba
wanglolly <wanglolly@gmail.com>
2018-05-11 17:21:27 +0800

    Debug CVAE.py

--- Scripts/CVAE.py
+++ Scripts/CVAE.py
@@ -117,6 +117,7 @@ def train(epoch, writer):
         label = label.view(-1, 1)
 
         optimizer.zero_grad()
+        print(data.size())
         recon_batch, mu, logvar, z = model(data, label)
         loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
