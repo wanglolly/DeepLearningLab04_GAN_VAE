1609806a824e1a85036cc4677eeac344f7f7ca07
wanglolly <wanglolly@gmail.com>
2018-05-13 00:42:59 +0800

    modify eval scripts

--- Scripts/infoGAN_eval.py
+++ Scripts/infoGAN_eval.py
@@ -56,7 +56,7 @@ for i in range(args.sets):
     #plot image  
     for p in range(10):
         plt.subplot(args.sets, 10, plotImageCount + 1)
-        plt.imshow(x[p].view(28,28).cpu().data.numpy(), norm = matplotlib.colors.Normalize(vmin = 0, vmax = 1))
+        plt.imshow(x[p].view(64,64).cpu().data.numpy(), norm = matplotlib.colors.Normalize(vmin = 0, vmax = 1))
         plt.axis('off')
         plotImageCount = plotImageCount + 1
 
